685b9e7db908c9fd993315214c1dbed3afa2bd3e
SEGAN <raoul.segan@troxgroup.com>
2025-08-31 16:52:23 +0200

    lecture 10, creating Customer and Order Service

--- Backend/API/Program.cs
+++ Backend/API/Program.cs
@@ -1,6 +1,8 @@
 using API.GraphQL;
+using Core.Interfaces;
 using GraphQL.Server.Ui.Voyager;
 using Infrastructure.Data;
+using Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
 
 var AllowSpecificOrigins = "_allowSpecificOrigins";
@@ -11,12 +13,25 @@ builder.Services.AddDbContextFactory<OMAContext>(options =>
 {
     options.UseInMemoryDatabase("InMemoryDb");
 });
-
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+builder.Services.AddScoped<IOrderService, OrderService>();
 //graphql
 builder.Services
     .AddGraphQLServer()
     .AddQueryType<Query>()
     .AddFiltering();
+
+//cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: AllowSpecificOrigins,
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
---
+++ Backend/Core/Interfaces/ICustomerService.cs
@@ -0,0 +1,8 @@
+using Core.Entities;
+namespace Core.Interfaces
+{
+    public interface ICustomerService
+    {
+        IQueryable<Customer> GetCustomersAndOrders();
+    }
+}
